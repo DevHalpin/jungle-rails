01186d3ed24c435426662900258f7bafa4c0c30b
DevHalpin <echidnablade@hotmail.com>
2020-10-15 18:18:43 -0700

    Update controllers to use new login logic

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,8 +4,21 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by_mail(params[:email])
+    # If the user exists AND the password entered is correct
+    if user && user.authenticate(params[:password])
+      # Save the user id inside the browser cookie
+      session[:user_id] = user.id
+      redirect_to '/'
+    else
+      #if not, send back to login form
+      redirect_to '/login'
+    end
   end
 
   def destroy
+    #delete cookie and redirect to login
+    session[:user_id] = nil
+    redirect_to '/login'
   end
 end
