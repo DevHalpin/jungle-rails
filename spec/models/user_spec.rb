35c59ed40e98d133f17e1a3898a322752cbb0d73
DevHalpin <echidnablade@hotmail.com>
2020-10-19 13:14:15 -0700

    Add tests for name and email

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -25,6 +25,21 @@ RSpec.describe User, type: :model do
       expect(subject).to_not be_valid
     end
 
+    it 'is not valid without a first name' do
+      subject.first_name = nil
+      expect(subject).to_not be_valid
+    end
+
+    it 'is not valid without a last name' do
+      subject.last_name = nil
+      expect(subject).to_not be_valid
+    end
+
+    it 'is not valid without an email' do
+      subject.email = nil
+      expect(subject).to_not be_valid
+    end
+
     it 'is valid with valid attributes' do
       expect(subject).to be_valid
     end
